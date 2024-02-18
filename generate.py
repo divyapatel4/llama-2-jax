93481c542278664037356204c481a211f7be3caf
Divya Patel <divyapatel0273@gmail.com>
2024-02-19 01:40:58 +0530

    updats

--- generate.py
+++ generate.py
@@ -23,6 +23,7 @@ def main():
     # top_p = 0.05
     max_len = 256
     print('Available devices:', jax.devices())
+    print('Total TPUs available:', jax.device_count())
     params = load_params_from_disk()
     print('Successfully loaded model parameters!')
 
